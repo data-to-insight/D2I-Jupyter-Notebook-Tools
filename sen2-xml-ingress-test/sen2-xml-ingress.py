b665628cc8408a8456c77c7be586a9646fa012a5
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2024-08-12 14:00:46 +0000

    sen2-2pandas most working mostly

--- sen2-xml-ingress-test/sen2-xml-ingress.py
+++ sen2-xml-ingress-test/sen2-xml-ingress.py
@@ -9,9 +9,7 @@ def get_values(xml_elements, table_dict: dict, xml_block):
     # st.write(table_dict)
     # st.write(xml_block)
     for element in xml_elements:
-        
         try:
-            
             table_dict[element] = xml_block.find(element).text
         except:
             table_dict[element] = pd.NA
@@ -117,6 +115,8 @@ class XMLtoCSV():
         for child in children.findall('Person'):      
             self.create_child(child)
 
+        self.named_plan = self.named_plan[self.named_plan['StartDate'].notna()].copy()
+
  
 
     def create_header(self, header):
@@ -175,13 +175,14 @@ class XMLtoCSV():
             requests_list.append(requests_dict)
 
             self.create_assessments(request)
-            # self.create_named_plan(person)
-            # self.create_active_plan(person)
+            self.create_active_plans(request)
 
         requests_df = pd.DataFrame(requests_list)
         self.requests = pd.concat(
             [self.requests, requests_df], ignore_index=True
         )
+
+        
     
     def create_assessments(self, request):
         assessment_list = []
@@ -191,6 +192,8 @@ class XMLtoCSV():
         assessments = request.findall('Assessment')
 
         for assessment in assessments:
+
+            # assessments
             self.assessment_id += 1
             assessment_dict = {}
 
@@ -201,11 +204,105 @@ class XMLtoCSV():
             assessment_dict['assessment_id'] = self.assessment_id
             
             assessment_list.append(assessment_dict)
+
+            # named_plans
+        self.create_named_plan(assessment)
+
         
         assessment_df = pd.DataFrame(assessment_list)
         self.assessments = pd.concat(
             [self.assessments, assessment_df], ignore_index=True
         )
+
+    def create_named_plan(self, assessment):
+
+        named_plan_elements= [
+            'StartDate',
+            'PlanRes',
+            'PlanWPB',
+            'PB',
+            'OA',
+            'DP',
+            'CeaseDate',
+            'CeaseReason'
+            ]
+        named_plan_dict = {}
+
+        plan_detail_elements = [
+            'URN',
+            'UKPRN',
+            'SENSetting',
+            'SENSettingOther',
+            'PlacementRank',
+            'SENunitIndicator',
+            'ResourcedProvisionIndicator'
+        ]
+
+        named_plan_locs = assessment.find('NamedPlan')
+        plan_detail_list = []
+        
+        if named_plan_locs:
+            for plan_detail in named_plan_locs.findall('PlanDetail'):
+                named_plan_dict = get_values(named_plan_elements, named_plan_dict, named_plan_locs)
+                
+                named_plan_dict = get_values(plan_detail_elements, named_plan_dict, plan_detail)
+                
+                named_plan_dict['child_id'] = self.child_id
+                named_plan_dict['requests_id'] = self.requests_id
+                named_plan_dict['assessment_id'] = self.assessment_id
+
+                plan_detail_list.append(named_plan_dict)
+
+            named_plan_df = pd.DataFrame(plan_detail_list)
+            self.named_plan = pd.concat(
+                [self.named_plan, named_plan_df], ignore_index=True
+            )
+
+    def create_active_plans(self, request):
+        active_plans_list = []
+        active_plans_dict = {}
+        active_plan_elements = [
+            'TransferLA',
+            'RES',
+            'WPB',
+            'PlacementDetail',
+            'ReviewMeeting',
+            'ReviewOutcome',
+            'LastReview'
+        ]
+        placement_detail_elements = [
+            'URN',
+            'SENSettingOther',
+            'PlacementRank',
+            'EntryDate',
+            'LeavingDate',
+            'SENunitIndicator',
+            'ResourcedProvisionIndicator',
+        ]
+        sen_need_elements = [
+            'SENtype',
+            'SENtypeRank'
+        ]
+
+        active_plan_locs = request.find('ActivePlans')
+        if active_plan_locs:
+            placement_detail_locs = active_plan_locs.findall('PlacementDetail')
+            sen_need_locs = request.findall('SENneed')
+            
+            for plan_detail in placement_detail_locs:   
+                active_plans_dict = get_values(active_plan_elements, active_plans_dict, active_plan_locs)
+                active_plans_dict = get_values(placement_detail_elements, active_plans_dict, plan_detail)
+                active_plans_dict = get_values(sen_need_elements, active_plans_dict, sen_need_locs)
+                active_plans_dict['child_id'] = self.child_id
+                active_plans_dict['requests_id'] = self.requests_id
+
+                active_plans_list.append(active_plans_dict)
+
+            active_plan_df = pd.DataFrame(active_plans_list)
+            self.active_plans = pd.concat(
+                [self.active_plans, active_plan_df], ignore_index=True
+            )
+                
 
 
 def convert_data(root: ET.Element):
@@ -223,3 +320,5 @@ if file:
     st.write(data_files.persons)
     st.write(data_files.requests)
     st.write(data_files.assessments)
+    st.write(data_files.named_plan)
+    st.write(data_files.active_plans)
