17b53ff3cb556a2f0b2bd73f99916ceac19ab1ea
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2025-05-20 13:44:20 +0000

    add slices

--- choropleth/example_bar.py
+++ choropleth/example_bar.py
@@ -3,6 +3,18 @@ import plotly.express as px
 
 df = pd.read_csv('/workspaces/D2I-Jupyter-Notebook-Tools/choropleth/header.csv')
 
+# chne = df['ETHNIC'] == 'CHNE'
+# bracknell = df['LA'] == 'Bracknell Forest'
+
+# print(chne)
+
+# df_chne = df[(chne & bracknell) | (df['ETHNIC'] == 'BAFR')]
+
+# df_c_b = df[df['ETHNIC'].isin(['CHNE', 'BAFR'])]
+
+# mother_df = df[df['MOTHER'].notna()]
+# not_mother_df = df[df['MOTHER'].isna()]
+
 child_per_la = df.groupby('LA').size().to_frame('Count').reset_index()
 
 fig = px.bar(child_per_la, 
