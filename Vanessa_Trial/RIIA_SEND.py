42dfd72ff95a508697ad1b9e8e5ea8586ec00fd3
Vane92S <vanessa_smith9233@yahoo.com>
2026-01-30 14:42:36 +0000

    reading new files

--- Vanessa_Trial/RIIA_SEND.py
+++ Vanessa_Trial/RIIA_SEND.py
@@ -1,3 +1,8 @@
 import pandas as pd
-df = pd.read_excel('/workspaces/D2I-Jupyter-Notebook-Tools/Vanessa_Trial/Statistical Neighbours.xlsx', sheet_name='Stats Neighbours')
-print(df)
+statne = pd.read_excel('/workspaces/D2I-Jupyter-Notebook-Tools/Vanessa_Trial/Statistical Neighbours.xlsx', sheet_name='Stats Neighbours')
+print(statne)
+region = pd.read_excel('/workspaces/D2I-Jupyter-Notebook-Tools/Vanessa_Trial/Statistical Neighbours.xlsx', sheet_name='Regions and MCA')
+print(region)
+# The files above only changes once Stats Neighbours updated
+ofsted = pd.read_excel('/workspaces/D2I-Jupyter-Notebook-Tools/Vanessa_Trial/ofsted_csc_send_overview.xlsx' ,usecols=['la_code','outcome_grade','previous_inspection_date'],engine='openpyxl')
+print(ofsted) #update ofsted file every quarter - needs to be downloaded from SEND OFSTED Inspections Scrapes (Rob)
